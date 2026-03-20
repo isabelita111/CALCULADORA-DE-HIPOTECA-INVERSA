5bfdab59348a9020ec7533dc2323cdfb9b933132
Emmanuel <betaalvarez1994@gmail.com>
2026-03-20 08:07:51 -0500

    Correccion consola

--- src/view/consola_hipoteca_inversa.py
+++ src/view/consola_hipoteca_inversa.py
@@ -1,15 +1,20 @@
-from src.model import logica_calculohipoteca_comentado
+import sys
+sys.path.append("src")
+from model.logica_calculohipoteca_comentado import credito, calculadora_hipoteca_inversa
+
+
+from model import logica_calculohipoteca_comentado
 from model.logica_calculohipoteca_comentado import credito
 
 
 
 valor_inmueble = int(input("Ingrese el valor del inmueble: "))
 
-tasa_capitalizacion = float(input("ingrese la tasa de capitalizacion del banco: "))/100
+tasa_capitalizacion = float(input("ingrese la tasa de capitalizacion del banco: "))
 
 plazo_simulacion = int(input("ingrese el plazo de simulacion:"))
 
-porcentaje_LTV = float(input(" ingrese el porcentaje LTV permitido: "))/100
+porcentaje_LTV = float(input(" ingrese el porcentaje LTV permitido: "))
 
 edad = int(input("Ingrese su edad: "))
 datos = credito(valor_inmueble, tasa_capitalizacion, plazo_simulacion, porcentaje_LTV, edad)
@@ -20,5 +25,8 @@ saldo_proyectado = logica_calculohipoteca_comentado.calculadora_hipoteca_inversa
 print(f"La cuota mensual es de: ${monto_mensual_recibido:.0f}")
 print(f"El total acumulado es de: ${Total_recibido_acumulado:.0f}")
 print(f"El saldo proyectado es de: ${saldo_proyectado:.0f}")
+
+
+
 
 

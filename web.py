5747643f2abf1335ecb51f6cbcbee20749a041cd
Emmanuel <betaalvarez1994@gmail.com>
2026-05-26 19:51:05 -0500

    WEB

--- web.py
+++ web.py
@@ -22,14 +22,14 @@ def crear_tabla():
 
 @app.route('/guardar_calculos')
 def guardar_calculos():
-    datos = credito(valor_inmueble=int(request.args["valor_inmueble"]), tasa_capitalizacion=float(request.args["tasa_capitalizacion"]), 
+    datos = credito(cedula = str(request.args["cedula"]),valor_inmueble=int(request.args["valor_inmueble"]), tasa_capitalizacion=float(request.args["tasa_capitalizacion"]), 
                     plazo_simulacion=int(request.args["plazo_simulacion"]), porcentaje_LTV=float(request.args["porcentaje_LTV"]), 
                     edad=int(request.args["edad"]))
     monto_mensual_recibido = f"{calculadora_hipoteca_inversa.calcular_monto_mensual_recibido(datos):.0f}"
     total_recibido_acumulado = f"{calculadora_hipoteca_inversa.calcular_total_recibido_acumulado(datos):.0f}"
     saldo_proyectado = f"{calculadora_hipoteca_inversa.calcular_saldo_proyectado(datos):.0f}"
     CalculosController.insertar(datos)
-    return render_template('cuotas.html', valor_inmueble=datos.valor_inmueble, tasa_capitalizacion=datos.tasa_capitalizacion, plazo_simulacion=datos.plazo_simulacion, porcentaje_LTV=datos.porcentaje_LTV, edad=datos.edad, monto_mensual_recibido=monto_mensual_recibido, total_recibido_acumulado=total_recibido_acumulado, saldo_proyectado=saldo_proyectado)
+    return render_template('cuotas.html', cedula = datos.cedula, valor_inmueble=datos.valor_inmueble, tasa_capitalizacion=datos.tasa_capitalizacion, plazo_simulacion=datos.plazo_simulacion, porcentaje_LTV=datos.porcentaje_LTV, edad=datos.edad, monto_mensual_recibido=monto_mensual_recibido, total_recibido_acumulado=total_recibido_acumulado, saldo_proyectado=saldo_proyectado)
 
 if __name__ == '__main__':
     app.run(debug=True)

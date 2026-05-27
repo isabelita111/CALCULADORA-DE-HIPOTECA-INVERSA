eff91f98d6f38aab93360ffc2264db2ce0a7b3ce
Emmanuel <betaalvarez1994@gmail.com>
2026-05-27 09:00:52 -0500

    web

---
+++ src/view/web/vista_calculos.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append("src")
+
+from flask import Blueprint
+from flask import render_template, request
+from model.logica_calculohipoteca_comentado import credito, calculadora_hipoteca_inversa
+from controller.calculos_controller import CalculosController
+
+blueprint = Blueprint("Vista_calculos",__name__, "templates")
+
+
+
+@blueprint.route('/')
+def datos():
+    return render_template('datos.html')
+@blueprint.route('/crear_tabla')
+def crear_tabla():
+    CalculosController.crear_tabla()
+    return "Tabla creada"
+
+
+
+@blueprint.route('/guardar_calculos')
+def guardar_calculos():
+    datos = credito(cedula = str(request.args["cedula"]),valor_inmueble=int(request.args["valor_inmueble"]), tasa_capitalizacion=float(request.args["tasa_capitalizacion"]), 
+                    plazo_simulacion=int(request.args["plazo_simulacion"]), porcentaje_LTV=float(request.args["porcentaje_LTV"]), 
+                    edad=int(request.args["edad"]))
+    monto_mensual_recibido = f"{calculadora_hipoteca_inversa.calcular_monto_mensual_recibido(datos):.0f}"
+    total_recibido_acumulado = f"{calculadora_hipoteca_inversa.calcular_total_recibido_acumulado(datos):.0f}"
+    saldo_proyectado = f"{calculadora_hipoteca_inversa.calcular_saldo_proyectado(datos):.0f}"
+    CalculosController.insertar(datos)
+    return render_template('cuotas.html', cedula = datos.cedula, valor_inmueble=datos.valor_inmueble, tasa_capitalizacion=datos.tasa_capitalizacion, plazo_simulacion=datos.plazo_simulacion, porcentaje_LTV=datos.porcentaje_LTV, edad=datos.edad, monto_mensual_recibido=monto_mensual_recibido, total_recibido_acumulado=total_recibido_acumulado, saldo_proyectado=saldo_proyectado)
+
+if __name__ == '__main__':
+    blueprint.run(debug=True)
+
--- web.py
+++ web.py
@@ -6,30 +6,13 @@ from flask import render_template, request
 from model.logica_calculohipoteca_comentado import credito, calculadora_hipoteca_inversa
 from controller.calculos_controller import CalculosController
 
+from view.web import vista_calculos
+
 app = Flask(__name__)
 
 
+app.register_blueprint(vista_calculos.blueprint)
 
-@app.route('/')
-def datos():
-    return render_template('datos.html')
-@app.route('/crear_tabla')
-def crear_tabla():
-    CalculosController.crear_tabla()
-    return "Tabla creada"
-
-
-
-@app.route('/guardar_calculos')
-def guardar_calculos():
-    datos = credito(cedula = str(request.args["cedula"]),valor_inmueble=int(request.args["valor_inmueble"]), tasa_capitalizacion=float(request.args["tasa_capitalizacion"]), 
-                    plazo_simulacion=int(request.args["plazo_simulacion"]), porcentaje_LTV=float(request.args["porcentaje_LTV"]), 
-                    edad=int(request.args["edad"]))
-    monto_mensual_recibido = f"{calculadora_hipoteca_inversa.calcular_monto_mensual_recibido(datos):.0f}"
-    total_recibido_acumulado = f"{calculadora_hipoteca_inversa.calcular_total_recibido_acumulado(datos):.0f}"
-    saldo_proyectado = f"{calculadora_hipoteca_inversa.calcular_saldo_proyectado(datos):.0f}"
-    CalculosController.insertar(datos)
-    return render_template('cuotas.html', cedula = datos.cedula, valor_inmueble=datos.valor_inmueble, tasa_capitalizacion=datos.tasa_capitalizacion, plazo_simulacion=datos.plazo_simulacion, porcentaje_LTV=datos.porcentaje_LTV, edad=datos.edad, monto_mensual_recibido=monto_mensual_recibido, total_recibido_acumulado=total_recibido_acumulado, saldo_proyectado=saldo_proyectado)
 
 if __name__ == '__main__':
     app.run(debug=True)
